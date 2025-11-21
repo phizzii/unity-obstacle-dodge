d9a5c6ea92fcc0b3f5c7755d8cf43814ae9ab77d
Sophie <109428682+phizzii@users.noreply.github.com>
2025-11-21 14:35:09 +0000

    adding to mover script, fixing cinemachine, adding walls

--- My proj/Assets/Scripts/Mover.cs
+++ My proj/Assets/Scripts/Mover.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 10f;
     void Start()
     {
         
@@ -11,6 +11,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, yValue, zValue);
     }
 }
