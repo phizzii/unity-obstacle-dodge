888e81f905c7217a15519dc9e82ded0dda1f4bea
Sophie <109428682+phizzii@users.noreply.github.com>
2025-12-11 18:28:23 +0000

    mesh renderer and rigid body
    adding rigidbody component to falling object. in script, ensuring at start that mesh renderer is set to false and rigidbody use gravity component is also set to false. then, at time.time (serialized field set) change both mesh rendered and use gravity in rigidbody to true

--- My proj/Assets/Scripts/Dropper.cs
+++ My proj/Assets/Scripts/Dropper.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField] float timeToWait = 2f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    MeshRenderer myMeshRenderer;
+
+    Rigidbody myRigidBody;
+
+
     void Start()
     {
-        
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myRigidBody = GetComponent<Rigidbody>();
+
+        myMeshRenderer.enabled=false;
+        myRigidBody.useGravity=false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Time.time > timeToWait)
         {
-            Debug.Log("lookout below");
+            myMeshRenderer.enabled=true;
+            myRigidBody.useGravity=true;
         }
     }
 }
