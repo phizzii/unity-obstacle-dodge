4686e1e904a334c6caf756b94b4826d9c1f4a6d4
Sophie <109428682+phizzii@users.noreply.github.com>
2025-12-11 18:52:03 +0000

    using tags to increment hit variable
    additionally fixing a bug where when the dropping box fell it would increment the hit as well as the count not being consistent over all objects (removed score script from all objects except the player object)

--- My proj/Assets/Scripts/Scorer.cs
+++ My proj/Assets/Scripts/Scorer.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class Scorer : MonoBehaviour
 {
     int hits = 0;
-    private void OnCollisionEnter(Collision other) {
-        hits ++;
-        Debug.Log("you bumped into a thing this many times: " + hits);
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "Hit")
+        {
+            hits ++;
+            Debug.Log("you bumped into a thing this many times: " + hits);   
+        }
     }
 }
