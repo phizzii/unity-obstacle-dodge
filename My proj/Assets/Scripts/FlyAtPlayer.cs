03232dfb763ec41ff40e6c5f72e28444c142c46e
Sophie <109428682+phizzii@users.noreply.github.com>
2025-12-12 10:27:10 +0000

    destroy projectiles
    created a script for all the projectiles so when they reach the target, the game object is deleted to avoid cluttering up the scene

--- My proj/Assets/Scripts/FlyAtPlayer.cs
+++ My proj/Assets/Scripts/FlyAtPlayer.cs
@@ -12,6 +12,20 @@ public class FlyAtPlayer : MonoBehaviour
 
     void Update()
     {
+        MoveToPlayer();
+        DestroyWhenReached();
+    }
+
+    void MoveToPlayer()
+    {
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime*projectileSpeed);
+    }
+
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
     }
 }
